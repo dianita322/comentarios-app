7751492ad397477857f48817a3eb896bd54e6126
Tenno322 <kevn1231@gmail.com>
2026-03-22 02:40:47 -0500

    Configuracion inicial de Supabase en entorno local

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Bocchi the Rock</Link>
+              <Link href={"/"}>Bocchichona the Rock</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
               </div>
@@ -33,7 +33,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Proyecto conectado a Supabase</h2>
+            <h2 className="font-medium text-xl mb-4">Día 2 - proyecto conectado a Supabase</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
         </div>
